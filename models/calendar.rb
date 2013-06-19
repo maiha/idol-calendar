684048e4c2a1465d33252c6d180bd2241424f4a8
maiha <maiha@wota.jp>
2013-06-20 05:46:10 +0900

    add label to Calendar,Event for cache

--- models/calendar.rb
+++ models/calendar.rb
@@ -1,5 +1,7 @@
 class Calendar < Sequel::Model
+  include Labeling
+
   one_to_many :events
   one_to_many :taggings
-  many_to_many :tags, :join_table => :taggings
+  many_to_many :tags, :join_table => :taggings  
 end
--- models/event.rb
+++ models/event.rb
@@ -1,4 +1,6 @@
 class Event < Sequel::Model
+  include Labeling
+
   unrestrict_primary_key
   many_to_one :calendar
 end
